0ab17c67e858c49eb5f98019684e6e90fc37d796
vjdbarayuga <vjdbarayuga@gmail.com>
2024-10-02 23:42:06 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+//IF STATEMENT
+
+// let age = 25;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// }
+
+//ELSE STATEMENT
+// let age = 25;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are not an adult");
+// }
+
+//ELSE IF STATEMENT
+// let grade = 85;
+// if (grade >= 90) {
+//     console.log("Excellent");
+// } else if (grade >= 80) {
+//     console.log("Good");
+// } else {
+//     console.log("Needs Improvement");
+// }
+
+// CONDITIONAL TERNARY OPERATOR
+// let grade = 25;
+// grade >= 90 ? console.log("Excellent") 
+// : grade >= 80 ? console.log ("Good")
+// : console.log("Needs Improvement");
+
+//SWITCH STATEMENT
+// let day = "Tuesday";
+// switch (day) {
+//     case "Monday":
+//         console.log("It is Monday");
+//         break;
+//     case "Tuesday":
+//         console.log("It is Tuesday");
+//         break;
+//         default:
+//     console.log("It is neither Monday or Tuesday or other Day");
+// }
+
+//FOR LOOP
+// for (let i = 0; i < 9; i++){
+//     console.log(i);
+// }
+
+//WHILE LOOP
+// let count = 0;
+// while (count < 8) {
+//     console.log(count);
+//     count++;
+// }
+
+// DO WHILE
+// let count = 0;
+// do {
+//     console.log(count);
+//     count ++;
+
+// } while (count < 10);
+
+// function showAlert(){
+//     alert("Welcome to DSA Javascript");
+// }
+
+// function askName() {
+//     let userName = prompt("What is your name?");
+//     if (userName){
+//         alert("Hello," + userName + ",how are you");
+//     } else {
+//         alert("You did not provide a name!");
+//     }
+// }
